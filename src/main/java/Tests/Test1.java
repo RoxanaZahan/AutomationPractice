c43304da62d613f7221493dfc2bc1881b639005c
RoxanaZahan <roxana.zahan@gmail.com>
2019-08-01 18:30:36 +0300

    random test comment

--- src/main/java/Tests/Test1.java
+++ src/main/java/Tests/Test1.java
@@ -4,6 +4,7 @@ import Utils.SeleniumDriver;
 import org.testng.annotations.Test;
 
 public class Test1 extends SeleniumDriver {
+//    random test comment
     @Test
     public void TestSearch() {
         goToUrl("https://staging.groupon.com/");
