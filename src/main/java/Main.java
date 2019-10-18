fae474e71db44a57bdd42fa9dff3a41581b0a445
Roxana Zahan <c_rzahan@groupon.com>
2019-10-18 14:03:58 +0300

    develop branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("whatever");
     }
+
+    private void whatever() {
+        System.out.println("Whatever 2 for PR");
+    }
 }
