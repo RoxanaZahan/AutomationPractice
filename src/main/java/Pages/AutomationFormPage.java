81a58e47a2a51198503e3fbcd80f55a0c24e9dd9
Roxana Zahan <c_rzahan@groupon.com>
2019-10-22 13:15:35 +0300

    Added test for Last Name

--- src/main/java/Pages/AutomationFormPage.java
+++ src/main/java/Pages/AutomationFormPage.java
@@ -10,13 +10,24 @@ public class AutomationFormPage {
 
     @FindBy(xpath = "//input[@name = 'firstname']")
     private WebElement firstName;
+    @FindBy(xpath = "//input[@id='lastname']")
+    private WebElement lastName;
 
     public void enterFirstName(String firstName) {
         final WebElement firstNameTab = this.waitMethods.waitForElementToBeVisible(this.firstName);
         firstNameTab.sendKeys(firstName);
     }
-     public String validateFirstName() {
+    public String validateFirstName() {
         final WebElement enteredFirstName = this.waitMethods.waitForElementToBeVisible(this.firstName);
         return enteredFirstName.getAttribute("value");
-     }
+    }
+
+    public void enterLastName(String lastName) {
+        final WebElement firstNameTab = this.waitMethods.waitForElementToBeVisible(this.lastName);
+        firstNameTab.sendKeys(lastName);
+    }
+    public String validateLastName() {
+        final WebElement enteredFirstName = this.waitMethods.waitForElementToBeVisible(this.lastName);
+        return enteredFirstName.getAttribute("value");
+    }
 }
